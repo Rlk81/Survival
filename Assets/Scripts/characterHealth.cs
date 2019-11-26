934cbf8191cd9cd7ffda583276152fa15806261d
jhj24 <jhj24@txstate.edu>
2019-11-26 17:50:36 -0600

    health bar

---
+++ Assets/Scripts/characterHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterHealth : MonoBehaviour
+{
+	public float MaxHealth{get; set;}
+	public float CurrentHealth{get; set;}
+
+	public UnityEngine.UI.Slider healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		MaxHealth = 20f;
+		CurrentHealth = MaxHealth;
+		healthBar.value = CalculateHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.X))
+			DealDamage(6);
+	}
+
+	void DealDamage(float damageValue){
+		CurrentHealth -= damageValue;
+		healthBar.value = CalculateHealth();
+
+		if(CurrentHealth <= 0)
+			Die();
+	}
+
+	float CalculateHealth(){
+		return CurrentHealth / MaxHealth;
+	}
+
+	void Die(){
+		CurrentHealth = 0;
+		Debug.Log("You died.");
+	}
+}
